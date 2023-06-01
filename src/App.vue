46c7fff8d500d26091282202009dd881f989836c
Sagar Yadav <sagaryadav17@outlook.com>
2023-06-01 15:49:13 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<script>
+import BlogPostCard from './components/BlogPostCard.vue';
+
+export default {
+    name: 'App',
+    data() {
+        return { blogPosts: null }
+    },
+    async mounted() {
+        let query = `
+        {
+            user(username: "${import.meta.env.VITE_HASHNODE_USERNAME}") {
+                publication {
+                    posts(page: 0) {
+                        slug
+                        title
+                        dateAdded
+                        brief
+                    }
+                }
+            }
+        }`;
+        try {
+            let res = await fetch('https://api.hashnode.com/', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({ query }),
+            });
+            res = await res.json();
+            const response = res.data;
+            this.blogPosts = response?.user?.publication?.posts;
+        } catch (error) {
+            console.log(error);
+        }
+    },
+};
+
+</script>
+
+
+
+<template>
+    <header aria-label="Page Header">
+        <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+            <div class="sm:flex sm:items-center sm:justify-between">
+                <div class="text-center sm:text-left">
+                    <h1 class="text-2xl font-bold text-gray-900 sm:text-3xl">
+                        Work in progress
+                    </h1>
+
+                    <p class="mt-1.5 text-sm text-gray-500">
+                        Meanwhile, you can check out some of my blog posts.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+        <div class="rounded overflow-hidden shadow-lg" v-for="post in blogPosts">
+            <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">{{ post.title }}</div>
+                <p class="text-gray-700 text-base">
+                    {{ post.brief }}
+                </p>
+            </div>
+            <div class="px-6 pt-4 pb-2">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    {{ post.dateAdded }}</span>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/BlogPostCard.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  props: {
+    data: {},
+  }
+}
+</script>
+
+<template>
+  <article class="rounded-xl bg-white p-4 ring ring-indigo-50 sm:p-6 lg:p-8">
+    <div class="flex items-start sm:gap-8">
+      <div>
+        <h3 class="mt-4 text-lg font-medium sm:text-xl">
+          <a href="" class="hover:underline"> {{ data.title }} </a>
+        </h3>
+
+        <p class="mt-1 text-sm text-gray-700">
+          {{ data.brief }}
+        </p>
+
+        <div class="mt-4 sm:flex sm:items-center sm:gap-2">
+          <div class="flex items-center gap-1 text-gray-500">
+            <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+            </svg>
+
+            <p class="text-xs font-medium">{{ data.dateAdded }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
+
